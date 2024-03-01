e248a0f2b7e5947f977a00d0795947385b34772b
Bill Johnson <Bill.Johnson@Carrix.com>
2024-02-29 16:43:09 -0800

    task 433 - initial completion of task

--- gate volume queries.sql
+++ gate volume queries.sql
@@ -415,12 +415,12 @@ FROM
 	GATE_TRANSACTIONS gt 
 JOIN sztp_to_len ON sztp_to_len.sztp_id = gt.ctr_sztp_id
 WHERE 
-	(EXTRACT (YEAR FROM gt.created) >= 2018) -- OR EXTRACT (YEAR FROM gt.created) = 2022)
+	(EXTRACT (YEAR FROM gt.created) = 2023 OR EXTRACT (YEAR FROM gt.created) = 2022)
 	--trunc(gt.created)  BETWEEN to_date('2023-01-01', 'YYYY-MM-DD') AND to_date('2023-01-31','YYYY-MM-DD')
-	--AND gt.ctr_nbr IS NOT NULL
+	-- AND gt.ctr_nbr IS NOT NULL
 	AND gt.tran_status = 'EIR'
-	--AND (gt.WTASK_ID = '1FULLOUT' OR gt.WTASK_ID = 'FULLIN'  OR gt.WTASK_ID = 'FULLOUT'
-		 --OR gt.WTASK_ID = 'MTIN' OR gt.WTASK_ID = 'MTINB' OR gt.WTASK_ID = 'MTOUT' OR gt.WTASK_ID = 'MTOUTB')
+	AND (gt.WTASK_ID = '1FULLOUT' OR gt.WTASK_ID = 'FULLIN'  OR gt.WTASK_ID = 'FULLOUT'
+		 OR gt.WTASK_ID = 'MTIN' OR gt.WTASK_ID = 'MTINB' OR gt.WTASK_ID = 'MTOUT' OR gt.WTASK_ID = 'MTOUTB')
 GROUP BY EXTRACT (YEAR FROM gt.created), EXTRACT (MONTH FROM gt.created)
 ORDER BY EXTRACT (YEAR FROM gt.created), EXTRACT (MONTH FROM gt.created)
 ;
@@ -471,7 +471,7 @@ JOIN sztp_to_len ON sztp_to_len.sztp_id = gt.ctr_sztp_id
 WHERE 
 	(EXTRACT (YEAR FROM gt.created) = 2023 OR EXTRACT (YEAR FROM gt.created) = 2022)
 	--trunc(gt.created)  BETWEEN to_date('2023-01-01', 'YYYY-MM-DD') AND to_date('2023-01-31','YYYY-MM-DD')
-	AND gt.ctr_nbr IS NOT NULL
+	--AND gt.ctr_nbr IS NOT NULL
 	AND gt.tran_status = 'EIR'
 	AND (gt.WTASK_ID = 'RDRAYIN' OR gt.WTASK_ID = 'RFULLIN' OR gt.WTASK_ID = 'RFULLOUT' OR gt.WTASK_ID = 'RMTIN'  OR gt.WTASK_ID = 'RMTOUT')
 GROUP BY EXTRACT (YEAR FROM gt.created), EXTRACT (MONTH FROM gt.created)
@@ -656,4 +656,306 @@ WHERE
 		 --OR gt.WTASK_ID = 'MTIN' OR gt.WTASK_ID = 'MTINB' OR gt.WTASK_ID = 'MTOUT' OR gt.WTASK_ID = 'MTOUTB')
 GROUP BY gt.wtask_id
 ORDER BY gt.wtask_id
+;
+
+-- Reorganizing this investigation to methodically catalog the wtask_id and then select using the union of wtask_id over MIT, ZLO, and PCT
+-- Query to get the comprehensive list of wtask_id for each terminal
+SELECT 
+	gt.wtask_id
+	, count(*)
+FROM
+	GATE_TRANSACTIONS gt 
+WHERE 
+	gt.tran_status = 'EIR'
+GROUP BY gt.wtask_id
+ORDER BY gt.wtask_id
+;
+
+--Queries to select gate volumes for each terminal
+--MIT
+SELECT 
+	gt.wtask_id
+	, count(*)
+FROM
+	GATE_TRANSACTIONS gt 
+WHERE 
+/*	(EXTRACT (YEAR FROM gt.created) = 2022 AND 
+			(EXTRACT (MONTH FROM gt.created) = 1 OR 
+			 EXTRACT (MONTH FROM gt.created) = 2 OR
+			 EXTRACT (MONTH FROM gt.created) = 4 OR
+			 EXTRACT (MONTH FROM gt.created) = 5 OR
+			 EXTRACT (MONTH FROM gt.created) = 6 OR
+			 EXTRACT (MONTH FROM gt.created) = 7 OR
+			 EXTRACT (MONTH FROM gt.created) = 8 OR
+			 EXTRACT (MONTH FROM gt.created) = 9) 
+		OR (EXTRACT (YEAR FROM gt.created) = 2023 AND
+				(EXTRACT (MONTH FROM gt.created) = 1 OR
+				 EXTRACT (MONTH FROM gt.created) = 2 OR
+				 EXTRACT (MONTH FROM gt.created) = 4 OR
+				 EXTRACT (MONTH FROM gt.created) = 5))) 
+	AND */ gt.tran_status = 'EIR'
+GROUP BY gt.wtask_id
+ORDER BY gt.wtask_id
+;
+
+-- MIT queries from above, modified
+--First the maingate
+WITH sztp_to_len AS
+	(SELECT 
+		sztp_id, len
+	FROM 
+		(SELECT 
+			t2.sztp_id
+			, t2.len
+			, t2.cnt
+			, ROW_NUMBER() OVER	(PARTITION BY t2.sztp_id ORDER BY t2.cnt DESC) AS rn
+		FROM 
+			(SELECT t1.*, et.name, est.name
+			FROM (
+			    SELECT
+			    	to_number(eq.sztp_eqsz_id) AS len
+			    	, eq.sztp_class
+			    	, eq.sztp_id
+			    	, eq.sztp_eqtp_id
+			    	, count(*) AS cnt
+			    FROM equipment eq
+			    WHERE eq.sztp_class = 'CTR' AND REGEXP_LIKE(eq.SZTP_EQSZ_ID, '^[0-9]+$')
+			    GROUP BY to_number(eq.sztp_eqsz_id), eq.sztp_class, eq.sztp_id, eq.sztp_eqtp_id
+			    --ORDER BY to_number(eq.sztp_eqsz_id), eq.sztp_class, eq.sztp_id, eq.sztp_eqtp_id
+			) t1 
+		JOIN equipment_types et ON t1.sztp_eqtp_id = et.id
+		JOIN EQUIPMENT_SIZE_TYPES est ON t1.sztp_id = est.id
+		WHERE NOT t1.sztp_eqtp_id = 'RT' AND NOT t1.sztp_eqtp_id = 'PP'
+		--ORDER BY t1.sztp_id
+		) t2 )
+	WHERE rn = 1)
+SELECT 
+	EXTRACT (YEAR FROM gt.created)
+	, EXTRACT (MONTH FROM gt.created)
+	, count(*) AS Moves
+	, sum(sztp_to_len.len)/20 AS TEUs
+	, count(DISTINCT gt.tv_gkey) AS TruckTurns
+FROM
+	GATE_TRANSACTIONS gt 
+JOIN sztp_to_len ON sztp_to_len.sztp_id = gt.ctr_sztp_id
+WHERE 
+	(EXTRACT (YEAR FROM gt.created) = 2023 OR EXTRACT (YEAR FROM gt.created) = 2022)
+	--trunc(gt.created)  BETWEEN to_date('2023-01-01', 'YYYY-MM-DD') AND to_date('2023-01-31','YYYY-MM-DD')
+	-- AND gt.ctr_nbr IS NOT NULL
+	AND gt.tran_status = 'EIR'
+	AND (gt.WTASK_ID = '1FULLOUT' OR gt.WTASK_ID = 'FULLIN'  OR gt.WTASK_ID = 'FULLOUT'
+		 OR gt.WTASK_ID = 'MTIN' OR gt.WTASK_ID = 'MTINB' OR gt.WTASK_ID = 'MTOUT' OR gt.WTASK_ID = 'MTOUTB'
+		 OR gt.WTASK_ID = 'RDRAYIN' OR gt.WTASK_ID = 'RFULLIN' OR gt.WTASK_ID = 'RFULLOUT' OR gt.WTASK_ID = 'RMTIN'  OR gt.WTASK_ID = 'RMTOUT')
+GROUP BY EXTRACT (YEAR FROM gt.created), EXTRACT (MONTH FROM gt.created)
+ORDER BY EXTRACT (YEAR FROM gt.created), EXTRACT (MONTH FROM gt.created)
+;
+
+-- Joined query
+WITH 
+	mit_query AS (
+		SELECT 
+			gt.ctr_nbr
+			, gt.ctr_sztp_id
+			, gt.gkey
+			, est.id AS sztp_id
+			, est.eqsz_id AS len
+		--	EXTRACT (YEAR FROM gt.created)
+		--	, EXTRACT (MONTH FROM gt.created)
+		--	, count(*) AS Moves
+		--	, sum(sztp_to_len.len)/20 AS TEUs
+		--	, count(DISTINCT gt.tv_gkey) AS TruckTurns
+		FROM
+			GATE_TRANSACTIONS gt 
+		JOIN equipment_size_types est ON est.id = gt.ctr_sztp_id
+		WHERE 
+			(EXTRACT (YEAR FROM gt.created) = 2022 OR EXTRACT (YEAR FROM gt.created) = 2023) 
+			--trunc(gt.created)  BETWEEN to_date('2023-01-01', 'YYYY-MM-DD') AND to_date('2023-01-31','YYYY-MM-DD')
+			-- AND gt.ctr_nbr IS NOT NULL
+			AND gt.tran_status = 'EIR'
+			AND (gt.WTASK_ID = '1FULLOUT' OR gt.WTASK_ID = 'FULLIN'  OR gt.WTASK_ID = 'FULLOUT'
+				 OR gt.WTASK_ID = 'MTIN' OR gt.WTASK_ID = 'MTINB' OR gt.WTASK_ID = 'MTOUT' OR gt.WTASK_ID = 'MTOUTB'
+				 OR gt.WTASK_ID = 'RDRAYIN' OR gt.WTASK_ID = 'RFULLIN' OR gt.WTASK_ID = 'RFULLOUT' OR gt.WTASK_ID = 'RMTIN'  OR gt.WTASK_ID = 'RMTOUT')
+		--GROUP BY EXTRACT (YEAR FROM gt.created), EXTRACT (MONTH FROM gt.created)
+		--ORDER BY EXTRACT (YEAR FROM gt.created), EXTRACT (MONTH FROM gt.created)
+		), joined_query AS (
+			SELECT 
+				mq.ctr_nbr AS mq_nbr
+				, mq.ctr_sztp_id AS mq_sztp1
+				, mq.sztp_id AS mq_sztp2
+				, mq.len AS mq_len
+				, gt.ctr_nbr AS gt_nbr
+				, gt.ctr_sztp_id AS gt_sztp
+				--EXTRACT (YEAR FROM gt.created)
+				--, EXTRACT (MONTH FROM gt.created)
+				--, count(*) AS Moves
+			FROM
+				GATE_TRANSACTIONS gt
+			FULL OUTER JOIN mit_query mq ON gt.gkey = mq.gkey
+			WHERE 
+				(EXTRACT (YEAR FROM gt.created) = 2022 OR EXTRACT (YEAR FROM gt.created) = 2023) 
+				AND gt.tran_status = 'EIR'
+				AND (gt.WTASK_ID = '1FULLOUT' OR gt.WTASK_ID = 'FULLIN'  OR gt.WTASK_ID = 'FULLOUT'
+					 OR gt.WTASK_ID = 'MTIN' OR gt.WTASK_ID = 'MTINB' OR gt.WTASK_ID = 'MTOUT' OR gt.WTASK_ID = 'MTOUTB'
+					 OR gt.WTASK_ID = 'RDRAYIN' OR gt.WTASK_ID = 'RFULLIN' OR gt.WTASK_ID = 'RFULLOUT' OR gt.WTASK_ID = 'RMTIN'  OR gt.WTASK_ID = 'RMTOUT')
+			--GROUP BY EXTRACT (YEAR FROM gt.created), EXTRACT (MONTH FROM gt.created)
+			--ORDER BY EXTRACT (YEAR FROM gt.created), EXTRACT (MONTH FROM gt.created)
+		)
+		SELECT mq_nbr, mq_sztp1, mq_sztp2, mq_len, gt_nbr, gt_sztp FROM joined_query jq WHERE (jq.mq_nbr IS NULL OR jq.gt_nbr IS null)
+;
+SELECT * FROM EQUIPMENT_SIZE_TYPES est ;
+
+-- Now the railgate
+WITH sztp_to_len AS
+	(SELECT 
+		sztp_id, len
+	FROM 
+		(SELECT 
+			t2.sztp_id
+			, t2.len
+			, t2.cnt
+			, ROW_NUMBER() OVER	(PARTITION BY t2.sztp_id ORDER BY t2.cnt DESC) AS rn
+		FROM 
+			(SELECT t1.*, et.name, est.name
+			FROM (
+			    SELECT
+			    	to_number(eq.sztp_eqsz_id) AS len
+			    	, eq.sztp_class
+			    	, eq.sztp_id
+			    	, eq.sztp_eqtp_id
+			    	, count(*) AS cnt
+			    FROM equipment eq
+			    WHERE eq.sztp_class = 'CTR' AND REGEXP_LIKE(eq.SZTP_EQSZ_ID, '^[0-9]+$')
+			    GROUP BY to_number(eq.sztp_eqsz_id), eq.sztp_class, eq.sztp_id, eq.sztp_eqtp_id
+			    --ORDER BY to_number(eq.sztp_eqsz_id), eq.sztp_class, eq.sztp_id, eq.sztp_eqtp_id
+			) t1 
+		JOIN equipment_types et ON t1.sztp_eqtp_id = et.id
+		JOIN EQUIPMENT_SIZE_TYPES est ON t1.sztp_id = est.id
+		WHERE NOT t1.sztp_eqtp_id = 'RT' AND NOT t1.sztp_eqtp_id = 'PP'
+		--ORDER BY t1.sztp_id
+		) t2 )
+	WHERE rn = 1)
+SELECT 
+	EXTRACT (YEAR FROM gt.created)
+	, EXTRACT (MONTH FROM gt.created)
+	, count(*) AS Moves
+	, sum(sztp_to_len.len)/20 AS TEUs
+	, count(DISTINCT gt.tv_gkey) AS TruckTurns
+FROM
+	GATE_TRANSACTIONS gt 
+JOIN sztp_to_len ON sztp_to_len.sztp_id = gt.ctr_sztp_id
+WHERE 
+	(EXTRACT (YEAR FROM gt.created) = 2023 OR EXTRACT (YEAR FROM gt.created) = 2022)
+	--trunc(gt.created)  BETWEEN to_date('2023-01-01', 'YYYY-MM-DD') AND to_date('2023-01-31','YYYY-MM-DD')
+	--AND gt.ctr_nbr IS NOT NULL
+	AND gt.tran_status = 'EIR'
+	AND (gt.WTASK_ID = 'RDRAYIN' OR gt.WTASK_ID = 'RFULLIN' OR gt.WTASK_ID = 'RFULLOUT' OR gt.WTASK_ID = 'RMTIN'  OR gt.WTASK_ID = 'RMTOUT')
+GROUP BY EXTRACT (YEAR FROM gt.created), EXTRACT (MONTH FROM gt.created)
+ORDER BY EXTRACT (YEAR FROM gt.created), EXTRACT (MONTH FROM gt.created)
+;
+
+--Simplified original MIT query
+SELECT 
+	EXTRACT (YEAR FROM gt.created)
+	, EXTRACT (MONTH FROM gt.created)
+	, count(*) AS Moves
+FROM
+	GATE_TRANSACTIONS gt 
+WHERE 
+	(EXTRACT (YEAR FROM gt.created) = 2023 OR EXTRACT (YEAR FROM gt.created) = 2022)
+	AND gt.tran_status = 'EIR'
+	AND (gt.WTASK_ID = '1FULLOUT' OR gt.WTASK_ID = 'FULLIN'  OR gt.WTASK_ID = 'FULLOUT'
+		 OR gt.WTASK_ID = 'MTIN' OR gt.WTASK_ID = 'MTINB' OR gt.WTASK_ID = 'MTOUT' OR gt.WTASK_ID = 'MTOUTB'
+		 OR gt.WTASK_ID = 'RDRAYIN' OR gt.WTASK_ID = 'RFULLIN' OR gt.WTASK_ID = 'RFULLOUT' OR gt.WTASK_ID = 'RMTIN'  OR gt.WTASK_ID = 'RMTOUT')
+GROUP BY EXTRACT (YEAR FROM gt.created), EXTRACT (MONTH FROM gt.created)
+ORDER BY EXTRACT (YEAR FROM gt.created), EXTRACT (MONTH FROM gt.created)
+;
+
+-- Works for MIT/ZLO
+SELECT 
+		EXTRACT (YEAR FROM gt.created) AS year
+		, EXTRACT (MONTH FROM gt.created) AS month
+		, count(*) AS Moves
+		, sum(est.eqsz_id)/20 AS TEUs
+		, count(DISTINCT gt.tv_gkey) AS TruckTurns
+	FROM
+		GATE_TRANSACTIONS gt 
+	JOIN equipment_size_types est ON est.id = gt.ctr_sztp_id
+	WHERE 
+		(EXTRACT (YEAR FROM gt.created) = 2022 OR EXTRACT (YEAR FROM gt.created) = 2023) 
+		AND gt.tran_status = 'EIR'  
+		AND (gt.wtask_id = '1FULLOUT' OR gt.WTASK_ID = 'BOOKOUT' OR gt.WTASK_ID = 'BOOKOUTB'  
+				OR gt.WTASK_ID = 'CHSIN' OR gt.WTASK_ID = 'CHSINB' OR gt.wtask_id = 'CHSOUT'
+				OR gt.WTASK_ID = 'FULLIN' OR gt.WTASK_ID = 'FULLINB' OR gt.WTASK_ID = 'FULLINO' OR gt.WTASK_ID = 'FULLOUT' OR gt.WTASK_ID = 'FULLOUTB'
+				OR gt.WTASK_ID = 'MTIN' OR gt.WTASK_ID = 'MTINB' OR GT.WTASK_ID = 'MTINO' OR  gt.WTASK_ID = 'MTOUT' OR gt.WTASK_ID = 'MTOUTB'
+				OR gt.wtask_id = 'POOLOUT'
+				OR gt.WTASK_ID = 'REPOOUT' OR gt.WTASK_ID = 'REPOOUTB'
+				OR GT.WTASK_ID = 'RAILIN' OR GT.WTASK_ID = 'RAILOUT' OR gt.wtask_id = 'RDRAYIN'
+				OR GT.WTASK_ID = 'RFULLIN' OR GT.WTASK_ID = 'RFULLOUT' 
+				OR GT.WTASK_ID = 'RMTIN' OR GT.WTASK_ID = 'RMTOUT'
+				OR gt.wtask_id = 'TRUCKOUT')
+	GROUP BY EXTRACT (YEAR FROM gt.created), EXTRACT (MONTH FROM gt.created)
+	ORDER BY EXTRACT (YEAR FROM gt.created), EXTRACT (MONTH FROM gt.created)
+;
+
+-- Now for PCT again
+SELECT 
+		EXTRACT (YEAR FROM gt.created) AS year
+		, EXTRACT (MONTH FROM gt.created) AS month
+		, count(*) AS Moves
+		, sum(est.eqsz_id)/20 AS TEUs
+		, count(DISTINCT gt.tv_gkey) AS TruckTurns
+	FROM
+		GATE_TRANSACTIONS gt 
+	JOIN equipment_size_types est ON est.id = gt.ctr_sztp_id
+	WHERE 
+		(EXTRACT (YEAR FROM gt.created) = 2022 OR EXTRACT (YEAR FROM gt.created) = 2023) 
+		AND gt.tran_status = 'EIR'  
+		AND (gt.wtask_id = '1FULLOUT' OR gt.WTASK_ID = 'BOOKOUT' OR gt.WTASK_ID = 'BOOKOUTB'  
+				OR gt.WTASK_ID = 'CHSIN' OR gt.WTASK_ID = 'CHSINB' 
+				OR gt.WTASK_ID = 'FULLIN' OR gt.WTASK_ID = 'FULLINB' OR gt.WTASK_ID = 'FULLINO' OR gt.WTASK_ID = 'FULLOUT' OR gt.WTASK_ID = 'FULLOUTB'
+				OR gt.WTASK_ID = 'MTIN' OR gt.WTASK_ID = 'MTINB' OR GT.WTASK_ID = 'MTINO' OR  gt.WTASK_ID = 'MTOUT' OR gt.WTASK_ID = 'MTOUTB'
+				OR gt.WTASK_ID = 'REPOOUT' OR gt.WTASK_ID = 'REPOOUTB'
+				OR GT.WTASK_ID = 'RAILIN' OR GT.WTASK_ID = 'RAILOUT' OR gt.wtask_id = 'RDRAYIN'
+				OR GT.WTASK_ID = 'RFULLIN' OR GT.WTASK_ID = 'RFULLOUT' 
+				OR GT.WTASK_ID = 'RMTIN' OR GT.WTASK_ID = 'RMTOUT'
+				OR gt.wtask_id = 'TRUCKOUT')
+	GROUP BY EXTRACT (YEAR FROM gt.created), EXTRACT (MONTH FROM gt.created)
+	ORDER BY EXTRACT (YEAR FROM gt.created), EXTRACT (MONTH FROM gt.created)
+;
+
+-- Works for PCT, TAM, T5
+WITH 
+	date_series AS (
+	  SELECT
+	    TO_DATE('2021-01-30', 'YYYY-MM-DD') + LEVEL - 1 AS date_in_series
+	  FROM dual
+	  CONNECT BY TO_DATE('2021-01-30', 'YYYY-MM-DD') + LEVEL - 1 <= to_date('2024-01-26', 'YYYY-MM-DD')
+	), weeks AS (
+		SELECT 
+			trunc(DATE_in_series) AS DAY
+			, ROW_NUMBER() OVER (ORDER BY date_in_series) AS enum
+			, trunc(6 + (ROW_NUMBER() OVER (ORDER BY date_in_series) - 1) / 7) AS PMA_week
+		FROM date_series
+	)
+SELECT 
+	weeks.PMA_week
+	, TRUNC(MIN(weeks.day)) AS start_day
+	, trunc(max(weeks.day)) AS end_day
+	, COALESCE (count(gt.tran_status), 0) AS Volume
+FROM weeks
+JOIN gate_transactions gt ON trunc(weeks.day) = trunc(gt.created) AND 
+		trunc(gt.created) BETWEEN TO_DATE('2021-01-30', 'YYYY-MM-DD') AND to_date('2024-01-26', 'YYYY-MM-DD') 
+		AND gt.tran_status = 'EIR'  
+		AND (gt.wtask_id = '1FULLOUT' OR gt.WTASK_ID = 'BOOKOUT' OR gt.WTASK_ID = 'BOOKOUTB'  
+				OR gt.WTASK_ID = 'CHSIN' OR gt.WTASK_ID = 'CHSINB' OR gt.wtask_id = 'CHSOUT'
+				OR gt.WTASK_ID = 'FULLIN' OR gt.WTASK_ID = 'FULLINB' OR gt.WTASK_ID = 'FULLINO' OR gt.WTASK_ID = 'FULLOUT' OR gt.WTASK_ID = 'FULLOUTB'
+				OR gt.WTASK_ID = 'MTIN' OR gt.WTASK_ID = 'MTINB' OR GT.WTASK_ID = 'MTINO' OR  gt.WTASK_ID = 'MTOUT' OR gt.WTASK_ID = 'MTOUTB'
+				OR gt.wtask_id = 'POOLOUT'
+				OR gt.WTASK_ID = 'REPOOUT' OR gt.WTASK_ID = 'REPOOUTB'
+				OR GT.WTASK_ID = 'RAILIN' OR GT.WTASK_ID = 'RAILOUT' OR gt.wtask_id = 'RDRAYIN'
+				OR GT.WTASK_ID = 'RFULLIN' OR GT.WTASK_ID = 'RFULLOUT' 
+				OR GT.WTASK_ID = 'RMTIN' OR GT.WTASK_ID = 'RMTOUT'
+				OR gt.wtask_id = 'TRUCKOUT')
+GROUP BY weeks.PMA_week
+ORDER BY weeks.pma_week
 ;
