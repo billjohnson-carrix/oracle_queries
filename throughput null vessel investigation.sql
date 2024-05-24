2ae6bf7263487d20f3f52b7dd358a4771773295e
Bill Johnson <160171223+billjohnson-carrix@users.noreply.github.com>
2024-05-24 14:44:48 -0700

    throughput null vessel id investigation

---
+++ throughput null vessel investigation.sql
@@ -0,0 +1,36 @@
+SELECT
+	sum (CASE WHEN eh.vsl_id IS NULL THEN 1 ELSE 0 END) AS NULL_values
+	, sum (CASE WHEN eh.vsl_id IS NOT NULL THEN 1 ELSE 0 END) AS non_NULL_values
+FROM equipment_history eh
+WHERE 
+	EXTRACT (YEAR FROM eh.posted) = 2023
+	AND (wtask_id = 'LOAD' OR wtask_id = 'UNLOAD')
+;
+
+SELECT
+	EXTRACT (MONTH FROM eh.posted) AS month
+	, sum (CASE WHEN eh.vsl_id IS NULL THEN 1 ELSE 0 END) AS eh_null_ids
+	, sum (CASE WHEN eh.vsl_id IS NOT NULL THEN 1 ELSE 0 END) AS eh_non_null_ids
+	, sum (CASE WHEN vv.vsl_id IS NULL THEN 1 ELSE 0 END) AS vv_null_ids
+	, sum (CASE WHEN vv.vsl_id IS NOT NULL THEN 1 ELSE 0 END) AS vv_non_null_ids
+	, sum (CASE WHEN v.id IS NULL THEN 1 ELSE 0 END) AS ves_null_ids
+	, sum (CASE WHEN v.id IS NOT NULL THEN 1 ELSE 0 END) AS ves_non_null_ids
+FROM equipment_history eh
+LEFT JOIN vessel_visits vv ON 
+	vv.vsl_id = eh.vsl_id
+	AND (vv.in_voy_nbr = eh.voy_nbr OR vv.out_voy_nbr = eh.voy_nbr)
+LEFT JOIN vessels v ON
+	eh.vsl_id = v.id
+WHERE 
+	EXTRACT (YEAR FROM eh.posted) = 2022
+	AND EXTRACT (MONTH FROM eh.posted) >= 2
+	AND (eh.wtask_id = 'LOAD' OR eh.wtask_id = 'UNLOAD')
+GROUP BY EXTRACT (MONTH FROM eh.posted)
+ORDER BY EXTRACT (MONTH FROM eh.posted)
+;
+
+SELECT 
+	*
+FROM vessel_visits vv
+WHERE vv.vsl_id = 'POLCHIL' AND (vv.in_voy_nbr = '325N' OR vv.out_voy_nbr = '325N')
+;
