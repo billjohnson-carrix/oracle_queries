4b390ba0cfaddbabbe6eaaa3e734fb1a639d2b7c
Bill Johnson <160171223+billjohnson-carrix@users.noreply.github.com>
2024-06-03 17:02:27 -0700

    query for Galens spreadsheet

--- zlo gate investigation.sql
+++ zlo gate investigation.sql
@@ -57,3 +57,39 @@ SELECT
 FROM gate_records 
 GROUP BY EXTRACT (MONTH FROM posted) 
 ORDER BY EXTRACT (MONTH FROM posted);
+
+--Query for Galen's spreadsheet
+WITH gate_events AS (
+	select te.id
+	from terminal_events te
+	where 
+		te.event_group in ( 'GATE')
+		and te.required is not null	
+), gate_records AS (
+	select 
+		eh.posted
+		, eh.eq_nbr
+		, eh.sztp_id
+		, est.eqsz_id / 20 AS LENGTH_teu
+		, round (est.eqsz_id / 20) AS int_teu
+	from equipment_history eh
+	JOIN equipment_size_types est ON
+		eh.sztp_id = est.id
+	where 
+		(EXTRACT (YEAR FROM eh.posted) = 2023
+		 OR EXTRACT (YEAR FROM eh.posted) = 2024)
+		and eh.removed is null
+		and eh.wtask_id in (SELECT id FROM gate_events)
+		and eh.eq_class = 'CTR'
+)
+SELECT 
+	to_char(trunc(gr.posted, 'MM'),'MM/DD/YYYY') AS analysis_month
+	, 'T5S' AS terminal_key
+	, count(*) AS gate_volume_total_moves
+	, sum (gr.length_teu) AS gate_volume_total_teu_precise
+	, sum (gr.int_teu) AS gate_volumne_total_teu_whole
+	, 'Oracle' AS Platform
+FROM gate_records gr
+GROUP BY trunc(gr.posted, 'MM') 
+ORDER BY trunc(gr.posted, 'MM')
+;
