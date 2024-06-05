6ade9cb42b39cc0960e762c8cd8d154f7cd317fb
Bill Johnson <160171223+billjohnson-carrix@users.noreply.github.com>
2024-06-05 16:49:56 -0700

    toward replicating Josephs raw crane worktimes

--- task 970 - STS productivity internal validation.sql
+++ task 970 - STS productivity internal validation.sql
@@ -108,6 +108,7 @@ WITH vessel_visits_of_interest AS (
 	LEFT JOIN equipment_history eh ON
 		eh.vsl_id = vvoi.vsl_id
 		AND (eh.voy_nbr = vvoi.in_voy_nbr OR eh.voy_nbr = vvoi.out_voy_nbr)
+	--ORDER BY vvoi.departure_time, vvoi.vsl_id, vvoi.in_voy_nbr, eh.crane_no, eh.posted
 ), components_by_move AS (
 	SELECT 
 		noise.vsl_id
@@ -147,4 +148,104 @@ ORDER BY
 	cbm.departure_time
 	, cbm.vsl_id
 	, cbm.in_voy_nbr
+;
+
+--Replicating Joseph's crane worktime approach
+WITH crane_move_batches AS (
+	SELECT
+		eh.vsl_id
+		, eh.voy_nbr
+		, eh.crane_no
+		, eh.posted
+		, count (DISTINCT eh.eq_nbr) AS moves
+	FROM equipment_history eh
+	WHERE
+		eh.crane_no IS NOT NULL 
+		AND upper(eh.crane_no) NOT LIKE 'TEST%'
+		AND eh.eq_class = 'CTR'
+		AND eh.wtask_id IN ('LOAD','UNLOAD','REHCD','REHCDT','REHDC','REHDCT','REHCC','REHCCT')
+	GROUP BY 
+		eh.vsl_id
+		, eh.voy_nbr
+		, eh.crane_no
+		, eh.posted
+), worktimes_1 AS (
+	SELECT
+		vv.vsl_id
+		, vv.in_voy_nbr
+		, vv.out_voy_nbr
+		, coalesce(vv.ata, vv.eta) AS arrival_time
+		, coalesce(vv.atd, vv.etd) AS departure_time
+		, vv.gross_hours
+		, vv.net_hours
+		, cmb.crane_no
+		, cmb.posted
+		, cmb.moves
+		, lead(cmb.posted) OVER (PARTITION BY cmb.crane_no ORDER BY cmb.posted ASC) AS next_posted
+	FROM vessel_visits vv
+	JOIN crane_move_batches cmb ON
+		cmb.vsl_id = vv.vsl_id
+		AND (cmb.voy_nbr = vv.in_voy_nbr OR cmb.voy_nbr = vv.out_voy_nbr)
+		AND cmb.posted BETWEEN coalesce(vv.ata, vv.eta) AND coalesce(vv.atd, vv.etd)
+	WHERE EXTRACT (YEAR FROM vv.atd) IN ('2022','2023')
+), worktimes_2 AS (
+	SELECT
+		wt1.*
+		, least(wt1.departure_time,COALESCE(wt1.next_posted,wt1.departure_time)) AS move_endtime
+		, (least(wt1.departure_time,COALESCE(wt1.next_posted,wt1.departure_time)) - wt1.posted) * 24 AS worktime
+	FROM worktimes_1 wt1
+)
+SELECT
+	wt2.vsl_id
+	, wt2.in_voy_nbr
+	, wt2.out_voy_nbr
+	, wt2.arrival_time
+	, wt2.departure_time
+	, wt2.gross_hours
+	, wt2.net_hours
+	, sum (wt2.moves)
+	, sum (wt2.worktime)
+FROM worktimes_2 wt2
+GROUP BY 
+	wt2.vsl_id
+	, wt2.in_voy_nbr
+	, wt2.out_voy_nbr
+	, wt2.arrival_time
+	, wt2.departure_time
+	, wt2.gross_hours
+	, wt2.net_hours
+ORDER BY 
+	wt2.departure_time
+	, wt2.vsl_id
+;
+
+SELECT 
+	eh.crane_no
+	, eh.posted
+FROM equipment_history eh 
+WHERE 
+	eh.vsl_id = 'MSCMONT' 
+	AND (eh.voy_nbr = '148A' OR eh.voy_nbr = '152R') 
+	AND eh.eq_class = 'CTR'
+ORDER BY
+	eh.crane_no
+	, eh.posted
+;
+
+SELECT * FROM vessel_visits WHERE vsl_id = 'MSCMONT' AND in_voy_nbr = '148A';
+
+SELECT
+	eh.vsl_id
+	, eh.voy_nbr
+	, eh.crane_no
+	, eh.posted
+FROM equipment_history eh
+WHERE
+	eh.posted BETWEEN to_date('2022-01-07','YYYY-MM-DD') AND to_date('2022-01-12','YYYY-MM-DD')
+	AND (eh.crane_no IS NULL OR eh.crane_no IN ('15','16','17','18')) 
+	AND eh.wtask_id IN ('LOAD','UNLOAD','REHCC','REHCCT','REHCD','REHCDT','REHDC','REHDCT')
+	AND eh.eq_class = 'CTR'
+ORDER BY 
+	eh.crane_no
+	, eh.posted
 ;
