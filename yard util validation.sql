1d72e1051755fae216656f25bc1c6fbcff0dd68c
Bill Johnson <160171223+billjohnson-carrix@users.noreply.github.com>
2024-06-28 15:59:08 -0700

    query for internal validation complete

--- yard util validation.sql
+++ yard util validation.sql
@@ -9,15 +9,13 @@ WITH
 			jn_datetime
 			, jn_entryid
 			, nbr
-			, sztp_class
+			, sztp_id
 			, loc_type
-			, to_number (sztp_eqsz_id) / 20 AS teu_precise
-			, CASE WHEN TO_number (sztp_eqsz_id) < 30 THEN 1 ELSE 2 END AS teu_whole
 		FROM equipment_jn
 		WHERE 
 			sztp_class = 'CTR'
-			AND EXTRACT (YEAR FROM jn_datetime) = 2024
-			AND EXTRACT (MONTH FROM jn_datetime) = 2
+			--AND EXTRACT (YEAR FROM jn_datetime) = 2024
+			--AND EXTRACT (MONTH FROM jn_datetime) = 2
 	/*	UNION ALL 
 		SELECT
 			jn_datetime
@@ -31,10 +29,10 @@ WITH
 		WHERE 
 			sztp_class = 'CTR'
 			AND jn_entryid >= 229820141*/
-		ORDER BY 
+/*		ORDER BY 
 			nbr
-			, jn_entryid
-		FETCH FIRST 100000 ROWS ONLY 
+			, jn_entryid*/
+--		FETCH FIRST 100000 ROWS ONLY 
 	), labeled_entries AS (
 		SELECT
 			jn_entries.*
@@ -60,9 +58,8 @@ WITH
 			jn_datetime
 			, jn_entryid
 			, nbr
+			, sztp_id
 			, loc_type
-			, teu_precise
-			, teu_whole
 			, effective
 			, expired
 			, entry_label
@@ -79,8 +76,7 @@ WITH
 ), yard_durations AS (
 		SELECT 
 			nbr
-			, teu_precise
-			, teu_whole
+			, sztp_id
 			, CASE 
 				WHEN entry_label = 'Enters' AND exit_label = 'Current' THEN jn_datetime
 				WHEN nbr = LEAD(nbr,1,NULL) OVER (PARTITION BY nbr ORDER BY jn_entryid) AND entry_label = 'Enters' THEN jn_datetime
@@ -107,32 +103,45 @@ WITH
 		/*ORDER BY 
 			nbr
 			, in_yard_datetime*/
-	), DateSeries (timestamp_col) AS (
-	    SELECT TO_TIMESTAMP('2024-02-01 23:35:00', 'YYYY-MM-DD HH24:MI:SS') AS timestamp_col
+	), DateSeries (date_col) AS (
+	    SELECT TO_date('2023-01-01', 'YYYY-MM-DD') AS date_col
 		    FROM dual
 	    UNION ALL
-	    SELECT timestamp_col + INTERVAL '1' DAY
+	    SELECT date_col + INTERVAL '1' DAY 
 		    FROM dateseries
-	    WHERE timestamp_col < to_date('2024-02-29','YYYY-MM-DD') + INTERVAL '00:35' HOUR TO MINUTE
+	    WHERE date_col < to_date('2024-06-30','YYYY-MM-DD')
 	), dates_of_interest AS (
 		SELECT 
-			timestamp_col
+			trunc(date_col) AS date_col
 		FROM 
 			DateSeries	
-	)
-SELECT 
-	doi.timestamp_col
-	, count(*)
-	, sum (acc.teu_precise) AS teu_precise
-	, sum (acc.teu_whole) AS teu_whole
-FROM dates_of_interest doi
-JOIN accum_snap acc ON 
-	doi.timestamp_col BETWEEN in_yard_datetime AND out_yard_datetime
-	OR (doi.timestamp_col >= in_yard_datetime AND out_yard_datetime IS NULL)
-	OR (doi.timestamp_col < out_yard_datetime AND in_yard_datetime IS null)
-GROUP BY timestamp_col
-ORDER BY timestamp_col
+	), container_lists AS (
+		SELECT 
+			trunc(doi.date_col) AS date_actual
+			, acc.nbr AS containers
+			, acc.sztp_id AS sztp
+		FROM dates_of_interest doi
+		JOIN accum_snap acc ON 
+			trunc(doi.date_col) BETWEEN trunc(in_yard_datetime) AND trunc(out_yard_datetime)
+			OR (trunc(doi.date_col) >= trunc(in_yard_datetime) AND out_yard_datetime IS NULL)
+			OR (trunc(doi.date_col) < trunc(out_yard_datetime) AND in_yard_datetime IS null)
+		GROUP BY trunc(doi.date_col), acc.nbr, acc.sztp_id
+		--ORDER BY trunc(doi.date_col)
+)--, daily_stats 
+SELECT
+	cl.date_actual
+	, count(cl.containers) AS container_count
+	, sum(to_number (substr(cl.sztp,1,2)) / 20) AS teu_precise
+	, sum(CASE WHEN to_number(substr(cl.sztp,1,2)) < 30 THEN 1 ELSE 2 END) AS teu_whole
+FROM container_lists cl
+GROUP BY cl.date_actual
+ORDER BY cl.date_actual
 ;
+
+--			, to_number (sztp_eqsz_id) / 20 AS teu_precise
+--			, CASE WHEN TO_number (sztp_eqsz_id) < 30 THEN 1 ELSE 2 END AS teu_whole
+
+SELECT to_number(substr('40DR',1,2)) FROM dual;
 
 SELECT 
 	*
@@ -140,3 +149,7 @@ FROM equipment_jn jn
 WHERE jn.nbr = 'AMFU3051495'
 ORDER BY jn.jn_entryid
 ;
+
+SELECT * FROM equipment_jn_arc FETCH FIRST 20 ROWS ONLY ;
+SELECT * FROM equipment_jn FETCH FIRST 20 ROWS ONLY ;
+SELECT * FROM equipment FETCH FIRST 20 ROWS ONLY ;
