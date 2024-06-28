4f5d260753762444f13d9b1ca13cdde72b5cfd52
Bill Johnson <160171223+billjohnson-carrix@users.noreply.github.com>
2024-06-27 17:34:15 -0700

    forgot to same

--- turn time external valid.sql
+++ turn time external valid.sql
@@ -7,6 +7,21 @@ WHERE
 	tv.entered IS NOT NULL 
 	AND tv.exited IS NOT NULL 
 	AND EXTRACT (YEAR FROM tv.entered) = 2024
-	AND EXTRACT (MONTH from tv.entered) = 5
---FETCH FIRST 100 ROWS ONLY
+	AND EXTRACT (MONTH from tv.entered) = 1
+	AND (tv.exited - tv.entered) * 24 * 60 >= 15
+	AND (tv.exited - tv.entered) * 24 * 60 <= 120
+;
+
+SELECT
+	avg(tv.exited - tv.entered) * 24 * 60 AS avg_turn_time
+	--tv.entered
+	--, tv.exited
+FROM truck_visits_arc tv
+WHERE
+	tv.entered IS NOT NULL 
+	AND tv.exited IS NOT NULL 
+	AND EXTRACT (YEAR FROM tv.entered) = 2024
+	AND EXTRACT (MONTH from tv.entered) = 1
+	--AND (tv.exited - tv.entered) * 24 * 60 >= 15
+	--AND (tv.exited - tv.entered) * 24 * 60 <= 120
 ;
