21761f1c62c9e8b4e2bb693a39d54c54f127f8cf
Bill Johnson <160171223+billjohnson-carrix@users.noreply.github.com>
2024-07-10 15:25:41 -0700

    small queries to verify how turn time is computed for MIT

--- turn time external valid.sql
+++ turn time external valid.sql
@@ -16,12 +16,39 @@ SELECT
 	avg(tv.exited - tv.entered) * 24 * 60 AS avg_turn_time
 	--tv.entered
 	--, tv.exited
-FROM truck_visits_arc tv
+FROM truck_visits tv
 WHERE
 	tv.entered IS NOT NULL 
 	AND tv.exited IS NOT NULL 
 	AND EXTRACT (YEAR FROM tv.entered) = 2024
 	AND EXTRACT (MONTH from tv.entered) = 1
-	--AND (tv.exited - tv.entered) * 24 * 60 >= 15
-	--AND (tv.exited - tv.entered) * 24 * 60 <= 120
 ;
+
+select 
+    tv.gkey
+    , tv.entered
+    , tv.exited
+    , (tv.exited - tv.entered) * 24 * 60 AS duration
+from truck_visits tv
+where
+    tv.entered IS NOT NULL 
+	AND tv.exited IS NOT NULL 
+	AND extract(YEAR FROM tv.entered) = 2024
+	AND extract(MONTH FROM tv.entered) = 1
+order by
+    4 DESC
+;
+
+select 
+    sum (CASE WHEN (tv.exited - tv.entered) * 24 * 60 > 1000 THEN 1 ELSE 0 END ) AS very_long_count
+    , count(*) AS count_all
+from truck_visits tv
+where
+    tv.entered IS NOT NULL 
+	AND tv.exited IS NOT NULL 
+	AND extract(YEAR FROM tv.entered) = 2024
+	AND extract(MONTH FROM tv.entered) = 1
+;
+
+SELECT * FROM truck_visits WHERE gkey = '9242496';
+SELECT gt.ctr_nbr FROM gate_transactions gt where gt.tv_gkey = '9252517';
